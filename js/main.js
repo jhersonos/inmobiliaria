cfab4d8b8e706b91bf920f3294ab7cb4215fc4fc
jherson onairam <jherson@MacBook-Pro-de-jherson.local>
2017-07-27 16:32:08 -0500

    init

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+
+  $('.mobile-menu ul li a').click(function(){
+    $(".mobile-nav-button").trigger('click')
+  })
+  window.addEventListener('scroll',function(){
+    if (this.scrollY > 50) {
+      $('#nav-head').addClass('white');
+    }else{
+      $('#nav-head').removeClass('white');
+    }
+  })
+
+  $("ul li a").click(function() {
+    var at = $(this).attr('href');
+      $('html, body').animate({
+          scrollTop: $(at).offset().top
+      }, 800);
+  }); 
+  $("#more").click(function(){
+    var at = $(this).attr('href');
+    $('html, body').animate({
+          scrollTop: $(at).offset().top
+      }, 800);
+  })
+  $("#contact").click(function(){
+    var at = $(this).attr('href');
+    $('html, body').animate({
+          scrollTop: $(at).offset().top
+      }, 800);
+  })
+  // $("html, body").animate({ scrollTop: $("#wrapper").scrollTop() }, 1000);
+
+  $('.mobile-nav-button').on('click', function() {
+  $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(1)" ).toggleClass( "mobile-nav-button__line--1");
+  $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(2)" ).toggleClass( "mobile-nav-button__line--2");  
+  $( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(3)" ).toggleClass( "mobile-nav-button__line--3");  
+  
+  $('.mobile-menu').toggleClass('mobile-menu--open');
+  return false;
+}); 
+});
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
